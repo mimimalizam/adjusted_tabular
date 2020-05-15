e24753879f505380d776f7a4d7ed7bdd6889a9fd
Milana Stojadinov <milana.stojadinov@gmail.com>
2020-05-15 23:04:24 +0200

    Employ Task.async_stream

--- lib/workers/foo.ex
+++ lib/workers/foo.ex
@@ -3,7 +3,7 @@ defmodule AdjustedTabular.Workers.Foo do
   require Logger
 
   def setup do
-    table_name = "source"
+    table_name = "chunks"
     columns_definition = "a integer, b integer, c integer"
 
     {:ok, pid, _} =
@@ -14,16 +14,41 @@ defmodule AdjustedTabular.Workers.Foo do
       )
 
     interval = 1..1_000_000
-    query = Postgrex.prepare!(pid, "", "INSERT INTO test(a, b, c) VALUES($1,$2,$3)")
+    chunk_size = 20
+
+    query =
+      Postgrex.prepare!(
+        pid,
+        "",
+        compose_query_string(chunk_size)
+      )
 
     Stream.zip([interval, Stream.cycle([1, 2, 0]), Stream.cycle([1, 2, 3, 4, 0])])
     |> Stream.map(&Tuple.to_list(&1))
-    |> Stream.map(fn values -> run_query_in_transaction(pid, query, values) end)
-    |> Enum.to_list()
+    |> Stream.chunk_every(chunk_size)
+    |> Task.async_stream(&process_chunk(&1, pid, query))
+    |> Stream.run()
+  end
+
+  def process_chunk(chunk, pid, query) do
+    run_query_in_transaction(pid, query, List.flatten(chunk))
   end
 
   def run_query_in_transaction(pid, query, values) do
     pid
     |> Postgrex.transaction(fn conn -> Postgrex.execute(pid, query, values) end)
+  end
+
+  def compose_query_string(params_count) do
+    params_string = draft_query_params(params_count)
+
+    "INSERT INTO test (a, b, c) VALUES #{params_string}"
+  end
+
+  def draft_query_params(n) do
+    Enum.reduce(2..n, "($1, $2, $3)", fn n, acc ->
+      a = (n - 1) * 3
+      acc <> ", ($#{a + 1}, $#{a + 2}, $#{a + 3})"
+    end)
   end
 end
