b8c51c7918ac521f5ad80989ce57a4b65859fa76
Milana Stojadinov <milana.stojadinov@gmail.com>
2020-05-16 09:05:34 +0200

    Update tests

--- test/adjusted_tabular/storage/query_test.exs
+++ test/adjusted_tabular/storage/query_test.exs
@@ -5,7 +5,7 @@ defmodule AdjustedTabular.Storage.QueryTest do
 
   describe "compose_insert_rows" do
     test "it returns correct insert query string" do
-      assert Query.compose_insert_rows(3) ==
+      assert Query.compose_insert_rows(3, "test") ==
                "INSERT INTO test (a, b, c) VALUES ($1, $2, $3), ($4, $5, $6), ($7, $8, $9)"
     end
   end
