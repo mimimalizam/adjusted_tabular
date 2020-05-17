6331d6ba268b1dbd04aaeb3c727ba7ec6fc4488b
Milana Stojadinov <milana.stojadinov@gmail.com>
2020-05-17 23:17:43 +0200

    Add db connections to supervision tree

--- lib/adjusted_tabular/storage/database.ex
+++ lib/adjusted_tabular/storage/database.ex
@@ -2,13 +2,10 @@ defmodule AdjustedTabular.Storage.Database do
   alias AdjustedTabular.Storage.Query
   @pg_env Application.get_env(:postgrex, :database_connection)
 
-  def connect(db_name) do
-    {:ok, pid} = env(database: db_name) |> Postgrex.start_link()
-  end
+  def conn_config(:foo), do: Keyword.merge(@pg_env, database: "foo", name: :foo_pid)
+  def conn_config(:bar), do: Keyword.merge(@pg_env, database: "bar", name: :bar_pid)
 
-  def set_up_table(table: table_name, db: db_name) do
-    {:ok, pid} = connect(db_name)
-
+  def set_up_table(table: table_name, pid: pid) do
     try do
       {:ok, pid, Query.create_table(pid, table_name)}
     rescue
@@ -30,6 +27,4 @@ defmodule AdjustedTabular.Storage.Database do
 
   defp parse_error(%Postgrex.Error{postgres: %{code: :duplicate_table}}), do: :table_exists
   defp parse_error(_), do: :unhandled_error
-
-  defp env(params \\ []), do: Keyword.merge(@pg_env, params)
 end
